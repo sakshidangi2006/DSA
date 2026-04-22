a366a32a357545c5d390e36555637c885a5fa9bd
Sakshidangi2006 <sakshidangi2006@gmail.com>
2026-04-22 13:16:13 +0530

    Middle_Element_of_Linked_List.cpp

---
+++ Slow_Fast_Pointer/Middle_Element_of_Linked_List.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x): val(x), next(NULL){};
+};
+
+ListNode* middleNode(ListNode* head) {
+        ListNode* slow = head;
+        ListNode* fast = head;
+        while(fast != NULL && fast->next != NULL) {
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        return slow;
+    }
+
+int main() {
+    ListNode* head = new ListNode(1);
+    head->next = new ListNode(2);
+    head->next->next = new ListNode(3);
+    head->next->next->next = new ListNode(4);
+
+    head = middleNode(head);
+    cout<<head->val;
+}
