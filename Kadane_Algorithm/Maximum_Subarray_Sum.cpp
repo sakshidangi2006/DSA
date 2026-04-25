5b2d8de50ed9e054b937f8b012be7164a6c265d7
Sakshidangi2006 <sakshidangi2006@gmail.com>
2026-04-25 19:45:19 +0530

    Kadane_Algorithm

---
+++ Kadane_Algorithm/Maximum_Subarray_Sum.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int maxSubArray(vector<int>& nums) {
+        int sum = nums[0];
+        int maxSum = nums[0];
+
+        for(int i = 1; i < nums.size(); i++) {
+            int val1 = nums[i];
+            int val2 = sum + nums[i];
+
+            sum = max(val1, val2);
+            maxSum = max(maxSum, sum);
+        }
+        return maxSum;
+    }
+
+
+int main() {
+    vector<int> a = {-2,1,-3,4,-1,2,1,-5,4};
+    int ans = maxSubArray(a);
+    cout << ans;
+    return 0;
+}
