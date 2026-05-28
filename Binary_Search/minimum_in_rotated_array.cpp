1ffafd4179ee3fa6c1e1d270365250935b7fccce
Sakshidangi2006 <sakshidangi2006@gmail.com>
2026-05-28 19:36:32 +0530

    minimum_in_rotated_array.cpp

---
+++ Binary_Search/minimum_in_rotated_array.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+
+int findMin(vector<int>& nums) {
+
+    int low = 0, high = nums.size() - 1;
+
+    while (low < high) {
+        int mid = low + (high - low) / 2;
+        if (nums[mid] > nums[high]) {
+            low = mid + 1;
+        }
+        else {
+            high = mid;
+        }
+    }
+    return nums[low];
+}
+
+int main() {
+    vector<int>nums = {3,4,5,1,2};
+    int ans = findMin(nums);
+    cout << ans;
+    return 0;
+}
