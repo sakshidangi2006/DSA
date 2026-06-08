cc4b30e96138baa5e9693ec40c12bfe7c88459b3
Sakshidangi2006 <sakshidangi2006@gmail.com>
2026-06-08 19:32:34 +0530

    minimum_cost_of_buy_candies_with_discount.cpp

---
+++ Daily_Challenges/minimum_cost_of_buy_candies_with_discount.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int minimumCost(vector<int>& cost) {
+    sort(cost.begin(), cost.end(), greater<int>());
+
+    int sum = 0;
+
+    for (int i = 0; i < cost.size(); i += 3) {
+        sum += cost[i];
+        if (i + 1 < cost.size())
+            sum += cost[i + 1];
+    }
+
+    return sum;
+}
+
+int main() {
+    vector<int>cost = {1,2,3};
+    int ans = minimumCost(cost);
+    cout << ans;
+    return 0;
+}
