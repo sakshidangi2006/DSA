d38b83164a758fc244a30a2855c02f14191e3c66
Sakshidangi2006 <sakshidangi2006@gmail.com>
2026-08-14 22:45:48 +0530

    change folder name

---
+++ Binary_Tree/102_level_order_traversal.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+};
+
+
+vector<vector<int>> levelOrder(TreeNode* root) {
+    vector<vector<int>> ans;
+    
+    if (root == nullptr) {
+        return ans;
+    }
+
+    queue<TreeNode*> q;
+    q.push(root);
+
+    while (!q.empty()) {
+        int levelSize = q.size();
+        vector<int> temp;
+
+        while (levelSize--) {
+            TreeNode* t = q.front();
+            q.pop();
+
+            temp.push_back(t->val);
+
+            if (t->left != nullptr) {
+                q.push(t->left);
+            }
+
+            if (t->right != nullptr) {
+                q.push(t->right);
+            }
+        }
+
+        ans.push_back(temp);
+    }
+
+    return ans;
+}
+
+int main() {
+    TreeNode* root = new TreeNode(3);
+    root -> left = new TreeNode(20);
+    root -> right = new TreeNode(9);
+    root -> right -> left = new TreeNode(15);
+    root -> right -> right = new TreeNode(7);
+
+    vector<vector<int>> ans = levelOrder(root);
+    cout<<"[";
+    for(auto &a : ans) {
+        cout<<"[";
+        for(int b : a) {
+            cout << b<<" ";
+        }
+        cout<<"] ";
+    }
+    cout<<"] ";
+    return 0;
+}
